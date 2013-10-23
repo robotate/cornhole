904c38a846cee1dd62c566a04b8f8353848edd61
root <root@ip-172-31-4-41.us-west-2.compute.internal>
2013-10-23 03:15:40 +0000

    begining to nodify

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,20 +1,16 @@
+fs = require('fs')
+
 function start(response, postData) {
   console.log("Request handler 'start' was called.");
-  var body = '<html>'+
-  '<head>'+
-  '<meta http-equiv="Content-Type" content="text/html; '+
-  'charset=UTF-8" />'+
-  '</head>'+
-  '<body>'+
-  '<form action="/upload" method="post">'+
-  '<textarea name="text" rows="20" cols="60"></textarea>'+
-  '<input type="submit" value="Submit text" />'+
-  '</form>'+
-  '</body>'+
-  '</html>';
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write(body);
-  response.end();
+  fs.readFile('./cornHole.html', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(data);
+    response.end();
+  });
 }
 
 function upload(response, postData) {
